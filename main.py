efff5652dae27698fa84830ce892d4b373a1b541
mcp5 <108768721+mcp5@users.noreply.github.com>
2022-07-05 18:34:49 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+calc_to_unit = 24*60*60
+name_of_unit = "seconds"
+
+print("20 days are " + str(50) + " minutes")
+print(f"20 days are {20*24*60} minutes")
+print(f"20 days are {20*calc_to_unit} {name_of_unit}")
+print(20*24*60)
